45062dc1eee6ebd0fca912fe27edd5fd1bb6f95c
faldibule <faldibule@gmail.com>
2021-11-24 16:40:29 +0700

    fixing bug pada paginate byUserId

--- middleware/paginate.js
+++ middleware/paginate.js
@@ -37,14 +37,14 @@ const paginate = {
         const result = {}
         result.currentPage = page;
         result.message = 'Data'
-        result.totalPage = Math.ceil(await model.countDocuments() / limit)
+        result.totalPage = Math.ceil(await model.countDocuments({user: userId}) / limit)
         result.data = await model.find({user: userId})
             .skip(startIndex)
             .limit(limit)
             .sort({ _id: -1 })
             .populate({ path: 'user', select: ['email', 'nama', 'username', 'image']})
             .exec();
-        if(endIndex < await model.countDocuments()){
+        if(endIndex < await model.countDocuments({user: userId})){
             result.next = {
                 page: page + 1,
                 limit
